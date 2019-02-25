0c7b52b9272fce54d80f189cbfeb2c9be0cb616f
imxood <imxood@gmail.com>
2019-02-26 01:14:49 +0800

    .

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 VER=4.17.0
-cd /v2ray
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-mkdir -p /programs/v2ray
-unzip v2ray.zip -d /programs/v2ray
-cp -f /v2ray/config.json /programs/v2ray
-chmod +x v2ray v2ctl
-sed -i "s/your_uuid/$UUID/g" config.json
-./v2ray
+UUID=$(curl https://www.uuidgenerator.net/version1/bulk?amount1=1)
+
+V2RAY_PATH="/programs/v2ray"
+V2RAY_CONFIG_PATH="/v2ray"
+
+wget -P $V2RAY_CONFIG_PATH -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+
+mkdir -p $V2RAY_PATH
+
+unzip v2ray.zip -d $V2RAY_PATH
+
+cp -f $V2RAY_CONFIG_PATH/config.json $V2RAY_PATH
+
+chmod +x $V2RAY_PATH/v2ray $V2RAY_PATH/v2ctl
+
+sed -i "s/UUID/$UUID/g" $V2RAY_CONFIG_PATH/config.json
+
+$V2RAY_PATH/v2ray
