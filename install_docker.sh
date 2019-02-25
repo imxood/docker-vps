72c2e1da4e90d341720605dbd1d760cbbb0f6342
imxood <imxood@gmail.com>
2019-02-26 00:54:06 +0800

    .

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt install -y curl
+sudo apt install -y curl git unzip
 curl -sSL https://get.daocloud.io/docker | sh
--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -3,15 +3,9 @@
 VER=4.17.0
 cd /v2ray
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip
-if [ ! -f "v2ray" ]; then
-  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
-  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
-  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
-  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
-fi
-
-cp -f /config.json .
+mkdir -p /programs/v2ray
+unzip v2ray.zip -d /programs/v2ray
+cp -f /v2ray/config.json /programs/v2ray
 chmod +x v2ray v2ctl
 sed -i "s/your_uuid/$UUID/g" config.json
 ./v2ray
