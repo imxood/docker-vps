1e4bdf62f86c68ff9ec9865ec8d2c6fc2c229f0e
imxood <imxood@gmail.com>
2019-02-28 23:02:25 +0800

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install docker and other tools
-if -z "$(which docker)"; then
+if [ -z "$(which docker)" ]; then
 	sudo apt install -y curl git unzip wget
 	curl -sSL https://get.daocloud.io/docker | sh
 fi
@@ -12,7 +12,7 @@ V2RAY_PROGRAM_PATH="$V2RAY_PATH/v2ray_program"
 
 mkdir -p $V2RAY_PATH && mkdir -p $V2RAY_PROGRAM_PATH
 
-if ! -f "$V2RAY_PATH/v2ray.zip"; then
+if [ ! -f "$V2RAY_PATH/v2ray.zip" ]; then
 	wget -P $V2RAY_CONFIG_PATH -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 fi
 
